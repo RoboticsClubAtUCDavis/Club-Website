776563d531b9f8b8191c370ce53f006b2b91437b
Zain Mustafa <108363829+ZainMGit@users.noreply.github.com>
2026-06-27 18:24:50 -0700

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://fastslash25.github.io/Davis-Robotics-Club-Site',
-  base: '/Davis-Robotics-Club-Site/',
+  site: 'https://RoboticsClubAtUCDavis.github.io',
+  base: '/Club-Website/',
   output: 'static',
 });
